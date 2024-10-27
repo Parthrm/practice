ec26758b84d0ac0ec6b475521ed94d6dae4feca8
Parthrm <parthrm2105@gmail.com>
2024-10-27 19:44:48 +0530

    solved problem 125

---
+++ 1-1000/101-200/125.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include "../../print.h" 
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=-1,j=s.length(),n=s.length();
+        while(i<j){
+            do{i++;} while (!isalnum(s[i]) and i<n-1);
+            do{j--;} while (!isalnum(s[j]) and j>0);
+            if(i>j) break;
+            if(tolower(s[i]) != tolower(s[j]))
+            return false;
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution s;
+    cout<<(s.isPalindrome("A man, a plan, a canal: Panama")); // true
+}
